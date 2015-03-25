6ecc3f60b63494caec94aa24977ccefdb7bd872a
Thomas Wang <wyatsky@gmail.com>
2015-03-25 18:57:46 +0800

    first ex today!

---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Thomas Wang'
+my_age = 23
+my_height = 173 # centimetres
+my_weight = 65 # kgs
+my_eyes = 'Black'
+my_teeth = "White"
+my_hair = 'Black'
+
+print "Let's talk about %s." % my_name
+print "He's %d cm tall." % my_height
+print "He's %d kg heave." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+print "If I add %d, %d, and %d I get %d." %(
+	my_age, my_height, my_weight, my_age + my_height + my_weight)
